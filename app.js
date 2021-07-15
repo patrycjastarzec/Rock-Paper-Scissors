0b1db456e2b8c3836abe659430b75fefaf703bb0
patrycjastarzec <patryc.sc@gmail.com>
2021-07-15 18:40:13 +0200

    Add function displaying the result

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ const computerPlay = () => {
     } else {
         computerSelection = SCISSORS;
     }
-    //console.log(`Computer selection: ${computerSelection}`);
     computerChoiceDiv.textContent = computerSelection;
     return computerSelection;
 }
@@ -35,7 +34,6 @@ const playerPlay = (item) => {
     let playerSelection;
     playerSelection = item;
     playerChoiceDiv.textContent = playerSelection;
-    //console.log(`Player selection: ${playerSelection}`);
     return playerSelection;
 }
 
@@ -55,100 +53,26 @@ const playRound = (playerInput, computerInput) => {
         playerScore++;
     } else if (playerInput == ROCK && computerInput == SCISSORS) {
         playerScore++;
-    }   
-    //console.log(`Player score: ${playerScore}`);
+    }
     playerScoreDiv.textContent = playerScore;
-    //console.log(`Computer score: ${computerScore}`);
     computerScoreDiv.textContent = computerScore;
+
+    const final = () => {
+      if (playerScore == 5) {
+        alert('Player won!'); 
+      } else if (computerScore == 5){
+        alert('Computer won!');
+      }
+    }
+    final();
 }
 
 // 4. Function that starts the game
 
 const selectItem = (item) => {
-    if (playerScore < 5 && computerScore < 5) {
-        playRound(playerPlay(item), computerPlay());
-    } else if (playerScore === 5) {
-        alert('Player won!');
-    } else if (computerScore === 5) {
-        alert('Computer won!');
-    } else if (playerScore === 5 && computerScore === 5) {
-        alert('It\'s a draw');
-    }
+    playRound(playerPlay(item), computerPlay());
 }
 
 rockButton.addEventListener("click", selectItem.bind(this, ROCK));
 paperButton.addEventListener("click", selectItem.bind(this, PAPER));
 scissorsButton.addEventListener("click", selectItem.bind(this, SCISSORS));
-
-
-
-
-
-/*
-const displayResult = () => {
-    while (computerScore < 6 || playerScore < 6) {
-        playGame(playerSelection, computerSelection);
-        console.log(`Computer score: ${computerScore}`);
-        console.log(`Player score: ${playerScore}`);
-    }
-    if (computerScore > playerScore) {
-        console.log('Computer won!');
-    }else if (computerScore < playerScore) {
-        console.log('Player won!');
-    } else {
-        console.log('It is a draw!');
-    }
-}
- */
-
-/*
-const selectRock = () =>{
-    playerSelection = ROCK;
-    console.log(`Player selection: ${playerSelection}`);
-    return playerSelection;
-}
-
-const selectPaper = () =>{
-    playerSelection = PAPER;
-    console.log(`Player selection: ${playerSelection}`);
-    return playerSelection;
-}
-
-const selectScissors = () =>{
-    playerSelection = SCISSORS;
-    console.log(`Player selection: ${playerSelection}`);
-    return playerSelection;
-}
-
-const userPlay = () => {
-   playerSelection = prompt("Please insert your choice: ROCK, PAPER or SCISSORS?");
-   playerSelection = playerSelection.toUpperCase();
-   if (playerSelection == ROCK || playerSelection == PAPER || playerSelection == SCISSORS) {
-    return playerSelection;
-   } else {
-       alert("You entered invalid input. Try again");
-   }
-}
-
-//userPlay();
-
-const playSingleRound = (playerInput, computerInput) => {
-    if (playerInput == ROCK && computerInput == PAPER) {
-        alert("You Lose! Paper beats rock");
-    } else if (playerInput == SCISSORS && computerInput == ROCK) {
-        alert("You Lose! Rock beats scissors");
-    } else if (playerInput == PAPER && computerInput == SCISSORS) {
-        alert("You Lose! Scissors beats paper");
-    } else if (playerInput == SCISSORS && computerInput == PAPER) {
-        alert("You Win! Scissors beats paper");
-    } else if (playerInput == PAPER && computerInput == ROCK) {
-        alert("You Win! Paper beats rock");
-    } else if (playerInput == ROCK && computerInput == SCISSORS) {
-        alert("You Win! Rock beats scissors");
-    } else if (playerInput == computerInput){
-        alert("It's a draw");
-    }    
-}
-
-playSingleRound(playerSelection, computerSelection);
-*/
